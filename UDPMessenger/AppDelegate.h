32d44b60e8fb200800495f9c701f39ee5fd62ce3
Pascal Frehner <frehnerp@gmail.com>
2011-12-31 10:01:57 +0100

    Initial Commit

---
+++ UDPMessenger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UDPMessenger
+//
+//  Created by Pascal Frehner on 31.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UDPMessenger/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UDPMessenger
+//
+//  Created by Pascal Frehner on 31.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
