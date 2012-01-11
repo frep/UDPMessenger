bcc13911309074a5bed17fae5b886b035f92d6a4
Pascal Frehner <frehnerp@gmail.com>
2012-01-11 23:44:07 +0100

    Navigation control view and third test view added

---
+++ UDPMessenger/RCoIPViewController.h
@@ -0,0 +1,19 @@
+//
+//  RCoIPViewController.h
+//  UDPMessenger
+//
+//  Created by Pascal Frehner on 11.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCoIPViewController : UIViewController
+{
+
+}
+
+
+
+
+@end
